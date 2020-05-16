3daea6fd3c059e85a9a44fc8fb75c15c9d8bfdb6
jhurl123 <justin@Justins-MBP.attlocal.net>
2020-05-16 18:52:01 -0400

    Procfile edit

--- src/server/server.js
+++ src/server/server.js
@@ -30,6 +30,6 @@ io.on('connection', function (client) {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
