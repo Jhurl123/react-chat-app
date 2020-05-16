4e8e126ac239f0b2edc1adcf87c20722d4af49f4
jhurl123 <justin@Justins-MBP.attlocal.net>
2020-05-16 19:24:21 -0400

    Path change

--- src/server/server.js
+++ src/server/server.js
@@ -31,5 +31,5 @@ io.on('connection', function (client) {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('../../../', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
